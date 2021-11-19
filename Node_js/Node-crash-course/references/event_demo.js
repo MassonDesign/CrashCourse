92725bb8c0287a62318fddcf52f61b68ed5c2055
frederic masson <fredericmasson@Air-de-frederic.hitronhub.home>
2021-11-19 14:39:17 -0500

    re arranged folder

---
+++ Node_js/Node-crash-course/references/event_demo.js
@@ -0,0 +1,13 @@
+const EventEmitter = require("events");
+
+// Create class
+class MyEmitter extends EventEmitter {}
+
+// init object
+const myEmitter = new MyEmitter();
+
+// Event listener
+myEmitter.on("event", () => console.log("Event fired"));
+
+// Init event
+myEmitter.emit("event");
---
+++ Node_js/Node-crash-course/references/url_demo.js
@@ -0,0 +1,30 @@
+const url = require("url");
+
+const myUrl = new URL("http://mywebsite.com/hello.html?id=100&status=active");
+
+// Serialized URL
+console.log(myUrl.href);
+
+// Host (root domain)
+console.log(myUrl.host);
+
+// Hostname
+console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialized query
+console.log(myUrl.search);
+
+// Params object
+console.log(myUrl.searchParams);
+
+// Add params
+myUrl.searchParams.append("abc", "123");
+console.log(myUrl.searchParams);
+
+// Loop trought params
+myUrl.searchParams.forEach((value, name) => {
+	console.log(`${name}: ${value}`);
+});
