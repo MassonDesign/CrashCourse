0b990b7d92213befb911c9f91b6d6ff2e764d46d
frederic masson <fredericmasson@MacBook-Air-de-frederic.local>
2022-06-15 19:59:16 -0400

    vue 3 course

---
+++ Vue_Js/vue-blog/src/components/SinglePost.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="post">
+		<router-link :to="{ name: 'Details', params: { id: post.id } }">
+			<h3>
+				{{ post.title }}
+			</h3>
+		</router-link>
+		<p>{{ snippet }}</p>
+		<span v-for="tag in post.tags" :key="tag"> #{{ tag }} </span>
+	</div>
+</template>
+
+<script>
+import { computed } from "@vue/runtime-core";
+export default {
+	props: ["post"],
+	setup(props) {
+		const snippet = computed(() => {
+			return props.post.body.substring(0, 100) + "...";
+		});
+
+		return { snippet };
+	},
+};
+</script>
+
+<style></style>
---
+++ Vue_Js/vue-blog/src/views/Home.vue
@@ -0,0 +1,84 @@
+<template>
+	<div class="home">
+		<h1>Home</h1>
+		<div v-if="error">{{ error }}</div>
+
+		<div v-if="posts.lastIndexOf">
+			<PostList :posts="posts" />
+		</div>
+		<div v-else>Loading...</div>
+
+		<!-- <PostList v-if="showPosts" :posts="posts" />
+		<button @click="showPosts = !showPosts">Toggle posts</button>
+		<button @click="posts.pop()">Delete a post</button> -->
+
+		<!-- <h2>Refs</h2>
+		<p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+		<button @click="updateNinjaOne">Update ninja one</button>
+		<h2>Reactive</h2>
+		<p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+		<button @click="updateNinjaTwo">Update ninja two</button> -->
+		<!-- <input type="text" v-model="search" />
+		<p>Search team - {{ search }}</p>
+		<div v-for="name in matchingName" :key="name">{{ name }}</div>
+		<button @click="handleClick">Stop watching</button> -->
+	</div>
+</template>
+
+<script>
+//import { ref } from "vue";
+import PostList from "../components/PostList.vue";
+import getPosts from "../composables/getPosts";
+// @ is an alias to /src
+
+export default {
+	components: { PostList },
+	name: "Home",
+	setup() {
+		const { posts, error, load } = getPosts();
+
+		load();
+
+		return { posts, error };
+
+		// const showPosts = ref(true);
+
+		// return { posts, showPosts };
+
+		// const search = ref("");
+		// const names = ref(["mario", "yoshi", "luigi", "toad", "browser", "peach"]);
+
+		// const stopWatch = watch(search, () => {
+		// 	console.log("watch function ran");
+		// });
+
+		// const stopEffect = watchEffect(() => {
+		// 	console.log("watcheffet function ran", search.value);
+		// });
+
+		// const matchingName = computed(() => {
+		// 	return names.value.filter((name) => name.includes(search.value));
+		// });
+
+		// const handleClick = () => {
+		// 	stopWatch();
+		// 	stopEffect();
+		// };
+
+		// return { names, search, matchingName };
+
+		// const ninjaOne = ref({ name: "mario", age: 30 });
+		// const ninjaTwo = reactive({ name: "luigi", age: 35 });
+
+		// const updateNinjaOne = () => {
+		// 	ninjaOne.value.age = 40;
+		// };
+
+		// const updateNinjaTwo = () => {
+		// 	ninjaTwo.age = 40;
+		// };
+
+		// return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
+	},
+};
+</script>
